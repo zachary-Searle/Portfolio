959aa95e96ea2a751490bc1d3dcadef8f039c13a
zachary-Searle <47543381+zachary-Searle@users.noreply.github.com>
2020-08-06 22:59:53 -0600

    Delete assn1.c

--- Input on linux command line/assn1/assn1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, char* argv[]){
-	printf("This program prints all the arguments passed to the function\n");
-	for(int i = 0; i < argc; i++){
-		printf("Argument %d:%s \n", i, argv[i]);
-	}
-	printf("The number of arguments passed was %d \n", argc);
-	return 0;
-}
